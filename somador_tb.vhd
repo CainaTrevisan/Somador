4e0daf108950beccd9215317f3d3d0b5f09c18a3
CainaTrevisan <cainact@gmail.com>
2015-08-20 19:46:07 -0300

    Ultima Versao

--- somador_tb.vhd
+++ somador_tb.vhd
@@ -4,14 +4,14 @@ use IEEE.std_logic_1164.all;
 entity somador_tb is
 end somador_tb;
 
-architecture arch_somador_tb of somador_tb is
+architecture arch_somador of somador_tb is
 
---	component somador is
---		port( 
---				clk, rst : in std_logic;
---				float1, float2: in std_logic_vector(31 downto 0);
---				result: out std_logic_vector(31 downto 0));
---	end component somador;
+	component somador is
+		port( 
+				clk, rst : in std_logic;
+				float1, float2: in std_logic_vector(31 downto 0);
+				result: out std_logic_vector(31 downto 0));
+	end component somador;
 
 	signal clock: std_logic := '0';
 	signal reset: std_logic := '1'; 
@@ -25,6 +25,6 @@ begin
 	f1 <= "00111111110000000000000000000000" after 500 NS;
 	f2 <= "01000000001000000000000000000000" after 500 NS;
 
-	A: entity work.somador port map (clock, reset, f1, f2, res );
+	A: somador port map (clock, reset, f1, f2, res );
 
-end arch_somador_tb;
+end arch_somador;
